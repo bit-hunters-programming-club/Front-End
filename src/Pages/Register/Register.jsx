4d651b0663183ae05f90317ce2fd1fab322cf839
BrayanPr <6519150032@utch.edu.mx>
2021-08-31 21:37:36 -0600

    register component folder created

---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import "./Regitser.css"
+
+class Regitser extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>hola</p>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Regitser;
