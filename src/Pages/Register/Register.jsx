254e879bb179aa3f84508570858184a7a5b2e60e
BrayanPr <6519150032@utch.edu.mx>
2021-09-01 10:19:31 -0600

    form created and styled

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -6,7 +6,19 @@ class Regitser extends React.Component {
     render() {
         return (
             <React.Fragment>
-
+                <div className="Container">
+                    <div className="Form__Header">
+                        <img className="User_Icon" src="https://image.flaticon.com/icons/png/512/1946/1946429.png"/>
+                        <h1 className="Header__Text">User info</h1>
+                    </div>
+                    <div className="Form">
+                        <input type="text" className="Form__Input" name="first_name" placeholder="name"/>
+                        <input type="text" className="Form__Input" name="last_name" placeholder="last name"/>
+                        <input type="email" className="Form__Input" name="email" placeholder="email"/>
+                        <input type="password" className="Form__Input" name="password" placeholder="password"/>
+                        <input type="password" className="Form__Input" name="password_confirm" placeholder="confirm password"/>
+                	</div>
+                </div>
             </React.Fragment>
         );
     }
