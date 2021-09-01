d4b70ba410be3934121c3931915394a5fd1d902f
BrayanPr <6519150032@utch.edu.mx>
2021-08-31 21:38:46 -0600

    Register added to app.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react' 
 import{ BrowserRouter, Route, Switch} from "react-router-dom"
 import AboutUs from "./Pages/AboutUs/AboutUs"
-
+import Register from "./Pages/Register/Register"
 function App() {
   return (
     <BrowserRouter>
