2b0746cc2b6d75c24946fb33bfa0816adae21089
BrayanPr <6519150032@utch.edu.mx>
2021-08-31 21:42:40 -0600

    some sintax errors fixed

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-import "./Regitser.css"
+import "./Register.css"
 
 class Regitser extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <p>hola</p>
+
             </React.Fragment>
         );
     }
